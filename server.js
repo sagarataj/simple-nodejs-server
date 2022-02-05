ecade0a6bfa77b4052ecc198dc33bf2760f45416
sagarataj <88649396+sagarataj@users.noreply.github.com>
2022-02-06 04:27:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var PORT = process.env.PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(PORT);
+console.log(`Server running on port ${PORT}`);
